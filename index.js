b1d42c812eb221ae7559b9a69ae00737e562a5d9
Mubeen-Anxari <mubeenansari10001@gmail.com>
2023-08-10 19:39:44 +0500

    first commit

---
+++ index.js
@@ -0,0 +1,22 @@
+"use strict";
+var time = 12;
+if (time >= 12 && time <= 17) {
+    console.log("Good Afternoon");
+}
+else if (time >= 18 && time <= 21) {
+    console.log("Good Evening");
+}
+else if (time >= 21 && time <= 24) {
+    console.log("Good Night");
+}
+else {
+    console.log("Good Morning");
+}
+var mail = "MubeenAnsari10001@gmail.com";
+var password = 1234567890;
+if ((password = 1234567890) && (mail = "MubeenAnsari10001@gmail.com")) {
+    console.log("Welcome to your email account");
+}
+else if ((password != 1234567890) && (mail != "MubeenAnsari10001@gmail.com")) {
+    console.log("Your email or password is incorrect");
+}
